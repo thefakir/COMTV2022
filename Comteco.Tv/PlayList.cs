90acc51833a3d787f76e62d1cdf1da3da7475fc4
Fidel Barcaya <msthefakir@gmail.com>
2022-08-08 12:52:52 -0400

    Adding more models

---
+++ Comteco.Tv/PlayList.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Comteco.Tv
+{
+    public class PlayList
+    {
+        private readonly List<Video> _videos;
+        public PlayList(List<Video> videos)
+        {
+            _videos = videos;
+        }
+    }
+    public class Video
+    {
+        public readonly string _title;
+        public readonly string _path;
+
+        public Video(string title, string path)
+        {
+            _title = title;
+            _path = path;
+        }
+    }
+
+}
